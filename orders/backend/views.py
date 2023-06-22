02886f673a7060c8cd9e773e8a950a2ff62ce3e2
Ivan Selukov <blawiken@ya.ru>
2023-06-22 12:52:53 +0500

    user register view

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.password_validation import validate_password
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import *
+from .serializers import *
+
+
+MSG_NO_REQUIRED_FIELDS = 'No required fields'
+
+class RegisterAccount(APIView):
+    def post(self, request):
+        if not {'email', 'password', 'username'}.issubset(request.data):
+            return Response({'Error': MSG_NO_REQUIRED_FIELDS})
+        
+        try:
+            validate_password(request.data['password'])
+        except Exception as error:
+            return Response({'Error': list(error)})
+        else:
+            request.data.update({})
+            user_serializer = UserSerializer(data=request.data)
+            if user_serializer.is_valid():
+                user = user_serializer.save()
+                user.set_password(request.data['password'])
+                user.save()
+                return Response(user.data)
+            
+            else:
+                return Response({'Error': user_serializer.errors})
