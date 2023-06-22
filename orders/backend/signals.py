757948a5a1be34b986e8d25acc32c3b554ba5dd8
Ivan Selukov <blawiken@ya.ru>
2023-06-22 13:24:02 +0500

    signals

---
+++ orders/backend/signals.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+
+from .models import User, ConfirmEmailToken
+
+__all__ = [
+    'new_user_registered_signal',
+]
+
+
+def new_user_registered_signal(user_id):
+    token, _ = ConfirmEmailToken.objects.get_or_create(user_id=user_id)
+    msg = EmailMultiAlternatives(
+        f'Token for {token.user.email}',
+        token.key,
+        settings.EMAIL_HOST_USER,
+        [token.user.email]
+    )
+    msg.send()
