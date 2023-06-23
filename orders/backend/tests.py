5b59e293a3c930d44dbc9d7f3080c3dabd500911
Ivan Selukov <blawiken@ya.ru>
2023-06-24 00:27:47 +0500

    some tests

--- orders/backend/tests.py
+++ orders/backend/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
+from .models import User
 
-# Create your tests here.
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+@pytest.fixture
+def admin():
+    return User.objects.create_user(email='admin@admin.admin', password='admin')
+
+@pytest.fixture
+def user_shop():
+    return User.objects.create_user(
+        email='df33@dsa.kz',
+        password='sd43fdsf',
+        username='user_test',
+        type='shop'
+    )
+
+@pytest.mark.django_db
+def user_login(client, user):
+    response = client.post(
+        f'user/login/',
+        data={
+            'email': 'df33@dsa.kz',
+            'password': 'sd43fdsf'
+        }
+    )
+    assert response.status_code == 201
